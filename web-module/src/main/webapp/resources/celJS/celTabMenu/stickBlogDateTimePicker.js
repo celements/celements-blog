a302fc1872d214c77f7b28fc26d501ba7804ff5c
darikhasham <dari.khasham@synventis.com>
2014-07-29 12:35:30 +0200

    Added DateTimePicker to the ArticleEdit

---
+++ web-module/src/main/webapp/resources/celJS/celTabMenu/stickBlogDateTimePicker.js
@@ -0,0 +1,10 @@
+jQuery("input").each(function( index, value ) {
+	var prefix = value.id.substr(0, value.id.indexOf('_'));
+	var suffix = value.id.substr(value.id.lastIndexOf('_')+1, value.id.length);
+	if(prefix == "XWiki.ArticleClass" && (suffix == "publishdate" || suffix == "archivedate")) {
+		jQuery("#"+value.id.replace(".", "\\.")).datetimepicker({	     
+			lang:'de',
+			dayOfWeekStart: 1,
+			format:'d/m/Y H:i:s'});
+	}
+});
