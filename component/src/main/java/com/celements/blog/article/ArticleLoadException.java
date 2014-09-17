6f436615ca5b3e7a3051620f27b0ce04e1192322
Marc Sladek <marc.sladek@synventis.com>
2014-09-17 22:23:57 +0200

    added more constructors for ArticleLoadException

--- component/src/main/java/com/celements/blog/article/ArticleLoadException.java
+++ component/src/main/java/com/celements/blog/article/ArticleLoadException.java
@@ -3,9 +3,17 @@ package com.celements.blog.article;
 public class ArticleLoadException extends Exception {
 
   private static final long serialVersionUID = 20140910181450L;
+
+  public ArticleLoadException(String msg) {
+    super(msg);
+  }
   
-  ArticleLoadException(Throwable cause) {
+  public ArticleLoadException(Throwable cause) {
     super(cause);
+  }
+
+  public ArticleLoadException(String msg, Throwable cause) {
+    super(msg, cause);
   }
 
 }
