ad4eee7dbf86adc77db1503f4e478cd4c584b8a4
Marc Sladek <marc.sladek@synventis.com>
2014-09-18 18:30:23 +0200

    added methods to BlogService: getBlogConfigDocRef, getBlogSpaceRef, isSubscribable, getSubribedToBlogs

--- component/src/main/java/com/celements/blog/service/BlogService.java
+++ component/src/main/java/com/celements/blog/service/BlogService.java
@@ -1,9 +1,13 @@
 package com.celements.blog.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.xwiki.component.annotation.Component;
@@ -12,6 +16,8 @@ import org.xwiki.component.manager.ComponentLookupException;
 import org.xwiki.component.manager.ComponentManager;
 import org.xwiki.context.Execution;
 import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.model.reference.SpaceReference;
+import org.xwiki.model.reference.WikiReference;
 import org.xwiki.query.Query;
 import org.xwiki.query.QueryException;
 import org.xwiki.query.QueryManager;
@@ -20,24 +26,32 @@ import com.celements.blog.article.Article;
 import com.celements.blog.article.ArticleLoadException;
 import com.celements.blog.article.ArticleSearchParameter;
 import com.celements.blog.article.IArticleEngineRole;
+import com.celements.blog.plugin.BlogClasses;
+import com.celements.common.classes.IClassCollectionRole;
 import com.celements.web.service.IWebUtilsService;
 import com.xpn.xwiki.XWikiContext;
 import com.xpn.xwiki.XWikiException;
 import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.objects.BaseObject;
 
 @Component
 public class BlogService implements IBlogServiceRole {
 
   private static Log LOGGER = LogFactory.getFactory().getInstance(BlogService.class);
   
+  private Map<SpaceReference, List<DocumentReference>> blogCache;
+  
   @Requirement
   private ComponentManager componentManager;
 
   @Requirement
-  private IWebUtilsService webUtilsService;
+  private QueryManager queryManager;
 
   @Requirement
-  private QueryManager queryManager;
+  private IWebUtilsService webUtils;
+  
+  @Requirement("celements.celBlogClasses")
+  private IClassCollectionRole blogClasses;
 
   @Requirement
   private Execution execution;
@@ -47,27 +61,24 @@ public class BlogService implements IBlogServiceRole {
         XWikiContext.EXECUTIONCONTEXT_KEY);
   }
 
+  @Deprecated
+  @Override
   public DocumentReference getBlogDocRefByBlogSpace(String blogSpaceName) {
     try {
-      String xwql = "from doc.object(Celements2.BlogConfigClass) as bconfig";
-      xwql += " where bconfig.blogspace = :blogSpaceName";
-      Query query = queryManager.createQuery(xwql, Query.XWQL);
-      query.bindValue("blogSpaceName", blogSpaceName);
-      List<String> blogList = query.setLimit(1).execute();
-      if (blogList.size() > 0) {
-        DocumentReference blogDocRef = webUtilsService.resolveDocumentReference(
-            blogList.get(0));
-        if (getContext().getWiki().exists(blogDocRef, getContext())) {
-          return blogDocRef;
-        }
-      }
-    } catch (QueryException queryExp) {
+      return getBlogConfigDocRef(webUtils.resolveSpaceReference(blogSpaceName, 
+          new WikiReference(getContext().getDatabase())));
+    } catch (QueryException qex) {
       LOGGER.error("Failed to parse xwql query to get BlogDocRef for blog space ["
-          + blogSpaceName + "].", queryExp);
+          + blogSpaceName + "].", qex);
+    } catch (XWikiException xwe) {
+      LOGGER.error("Failed to get blog document for blog space '" + blogSpaceName + "'", 
+          xwe);
     }
     return null;
   }
 
+  @Deprecated
+  @Override
   public XWikiDocument getBlogPageByBlogSpace(String blogSpaceName) {
     DocumentReference blogDocRef = getBlogDocRefByBlogSpace(blogSpaceName);
     if (blogDocRef != null) {
@@ -82,27 +93,113 @@ public class BlogService implements IBlogServiceRole {
   }
 
   @Override
-  public List<Article> getArticles(ArticleSearchParameter param
-      ) throws ArticleLoadException {
-    List<Article> articles;
-    if (param == null) {
-      // TODO create new param
+  public DocumentReference getBlogConfigDocRef(SpaceReference spaceRef
+      ) throws QueryException, XWikiException {
+    DocumentReference ret = null;
+    WikiReference wikiRef = new WikiReference(spaceRef.getParent());
+    List<DocumentReference> docRefs = getBlogCache(wikiRef).get(spaceRef);
+    if ((docRefs != null) && (docRefs.size() > 0)) {
+      ret = docRefs.get(0);
     }
-    IArticleEngineRole engine = getArticleEngine();
-    if (engine != null) {
-      articles = Collections.unmodifiableList(engine.getArticles(param));
-    } else {
-      articles = Collections.emptyList();
-    }
-    LOGGER.info("getBlogArticles: for " + param + " got " + articles.size() 
-        + " articles");
-    if (LOGGER.isTraceEnabled()) {
-      LOGGER.trace("getBlogArticles: for " + param + " got: " + articles);
-    }
-    return articles;
+    return ret;
   }
 
-  private IArticleEngineRole getArticleEngine() {
+  private synchronized Map<SpaceReference, List<DocumentReference>> getBlogCache(
+      WikiReference wikiRef) throws QueryException, XWikiException {
+    if (blogCache == null) {
+      Map<SpaceReference, List<DocumentReference>> map = 
+          new HashMap<SpaceReference, List<DocumentReference>>();
+      String xqwl = "from doc.object(" + BlogClasses.BLOG_CONFIG_CLASS + ")";
+      Query query = queryManager.createQuery(xqwl, Query.XWQL).setWiki(wikiRef.getName());
+      for (String result : query.<String>execute()) {
+        DocumentReference docRef = webUtils.resolveDocumentReference(result, wikiRef);
+        SpaceReference spaceRef = getBlogSpaceRef(docRef);
+        if (spaceRef != null) {
+          List<DocumentReference> list = map.get(spaceRef);
+          if (list == null) {
+            list = new ArrayList<DocumentReference>();
+            map.put(spaceRef, list);
+          }
+          list.add(docRef);
+        }
+      }
+      blogCache = Collections.unmodifiableMap(map);
+    }
+    return blogCache;
+  }
+
+  // TODO add listener to clear blog
+  public synchronized void clearBlogCache() {
+    if (blogCache != null) {
+      blogCache = null;
+    }
+  }
+
+  @Override
+  public SpaceReference getBlogSpaceRef(DocumentReference docRef) throws XWikiException {
+    SpaceReference spaceRef = null;
+    BaseObject confObj = getBlogConfigObject(docRef);
+    if (confObj != null) {
+      String spaceName = confObj.getStringValue(BlogClasses.PROPERTY_BLOG_CONFIG_BLOGSPACE);
+      if (StringUtils.isNotBlank(spaceName)) {
+        spaceRef = webUtils.resolveSpaceReference(spaceName, docRef.getWikiReference());
+      }
+    }
+    LOGGER.debug("getBlogSpace: resolved for '" + docRef + "' space:" + spaceRef);
+    return spaceRef;
+  }
+
+  @Override
+  public boolean isSubscribable(DocumentReference blogConfDocRef) throws XWikiException {
+    boolean isSubscribable = false;
+    BaseObject confObj = getBlogConfigObject(blogConfDocRef);
+    if (confObj != null) {
+      isSubscribable = confObj.getIntValue(
+          BlogClasses.PROPERTY_BLOG_CONFIG_IS_SUBSCRIBABLE, -1) == 1;
+    }
+    return isSubscribable;
+  }
+  
+  @Override
+  public List<DocumentReference> getSubribedToBlogs(DocumentReference blogConfDocRef
+      ) throws QueryException, XWikiException {
+    List<DocumentReference> ret = new ArrayList<DocumentReference>();
+    BaseObject confObj = getBlogConfigObject(blogConfDocRef);
+    if (confObj != null) {
+      String spaceNames = confObj.getStringValue(
+          BlogClasses.PROPERTY_BLOG_CONFIG_SUBSCRIBE_TO);
+      for (String spaceName : Arrays.asList(spaceNames.split(","))) {
+        if (StringUtils.isNotBlank(spaceName)) {
+          DocumentReference docRef = getBlogConfigDocRef(webUtils.resolveSpaceReference(
+              spaceName, blogConfDocRef.getWikiReference()));
+          if (isSubscribable(docRef)) {
+            ret.add(docRef);
+          }
+        }
+      }
+    }
+    return Collections.unmodifiableList(ret);
+  }
+
+  @Override
+  public List<Article> getArticles(DocumentReference blogConfDocRef, 
+      ArticleSearchParameter param) throws ArticleLoadException {
+    param = setParamtersForBlog(param, blogConfDocRef);
+    List<Article> articles = getArticleEngine().getArticles(param);
+    LOGGER.info("getArticles: for " + param + " got " + articles.size() + " articles");
+    if (LOGGER.isTraceEnabled()) {
+      LOGGER.trace("getArticles: for " + param + " got: " + articles);
+    }
+    return Collections.unmodifiableList(articles);
+  }
+
+  private ArticleSearchParameter setParamtersForBlog(ArticleSearchParameter param,
+      DocumentReference blogConfDocRef) throws ArticleLoadException {
+    // TODO
+    return param;
+  }
+
+  private IArticleEngineRole getArticleEngine() throws ArticleLoadException {
     IArticleEngineRole engine = null;
     String engineHint = getContext().getWiki().getXWikiPreference("blog_article_engine",
         "blog.article.engine", null, getContext());
@@ -116,9 +213,35 @@ public class BlogService implements IBlogServiceRole {
     } catch (ComponentLookupException exc) {
       LOGGER.error("Error looking up engine components", exc);
     }
-    LOGGER.info("getBlogArticleEngine: got engine '" + engine + "' for hint '" 
-        + engineHint + "'");
-    return engine;
+    if (engine != null) {
+      LOGGER.info("getBlogArticleEngine: got engine '" + engine + "' for hint '" 
+          + engineHint + "'");
+      return engine;
+    } else {
+      throw new ArticleLoadException("Unable to load engine for hint");
+    }
+  }
+  
+  private BaseObject getBlogConfigObject(DocumentReference blogConfDocRef
+      ) throws XWikiException {
+    BaseObject ret = null;
+    if (blogConfDocRef != null) {
+      XWikiDocument doc = getContext().getWiki().getDocument(blogConfDocRef, getContext());
+      ret = doc.getXObject(getBlogConfigClassRef(blogConfDocRef.getWikiReference()));    
+    }
+    return ret;
+  }
+  
+  DocumentReference getBlogConfigClassRef(WikiReference wikiRef) {
+    return ((BlogClasses) blogClasses).getBlogConfigClassRef(wikiRef.getName());
+  }
+  
+  void injectBlogCache(Map<SpaceReference, List<DocumentReference>> blogCache) {
+    this.blogCache = blogCache;
+  }
+
+  void injectQueryManager(QueryManager queryManager) {
+    this.queryManager = queryManager;
   }
 
 }
--- component/src/main/java/com/celements/blog/service/IBlogServiceRole.java
+++ component/src/main/java/com/celements/blog/service/IBlogServiceRole.java
@@ -4,27 +4,86 @@ import java.util.List;
 
 import org.xwiki.component.annotation.ComponentRole;
 import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.model.reference.SpaceReference;
+import org.xwiki.query.QueryException;
 
 import com.celements.blog.article.Article;
 import com.celements.blog.article.ArticleLoadException;
 import com.celements.blog.article.ArticleSearchParameter;
+import com.xpn.xwiki.XWikiException;
 import com.xpn.xwiki.doc.XWikiDocument;
 
 @ComponentRole
 public interface IBlogServiceRole {
 
+  /**
+   * @deprecated instead use {@link #getBlogConfigDocRef(SpaceReference)}
+   * 
+   * @param blogSpaceName
+   * @return
+   */
+  @Deprecated
   public DocumentReference getBlogDocRefByBlogSpace(String blogSpaceName);
 
+  /**
+   * @deprecated instead use {@link #getBlogConfigDocRef(SpaceReference)}
+   * 
+   * @param blogSpaceName
+   * @return
+   */
+  @Deprecated
   public XWikiDocument getBlogPageByBlogSpace(String blogSpaceName);
+  
+  /**
+   * gets the blog ref for the given space reference
+   * 
+   * @param spaceRef
+   * @return
+   * @throws QueryException 
+   * @throws XWikiException 
+   */
+  public DocumentReference getBlogConfigDocRef(SpaceReference spaceRef
+      ) throws QueryException, XWikiException;
+  
+  /**
+   * gets the space reference for the given blog ref
+   * 
+   * @param blogConfDocRef
+   * @return
+   * @throws XWikiException
+   */
+  public SpaceReference getBlogSpaceRef(DocumentReference blogConfDocRef
+      ) throws XWikiException;
+
+  /**
+   * checks whether blog ref is subscribable
+   * 
+   * @param blogConfDocRef
+   * @return
+   * @throws XWikiException
+   */
+  public boolean isSubscribable(DocumentReference blogConfDocRef) throws XWikiException;
+
+  /**
+   * gets all subscribed blog refs for the given blog ref
+   * 
+   * @param blogConfDocRef
+   * @return
+   * @throws QueryException
+   * @throws XWikiException
+   */
+  public List<DocumentReference> getSubribedToBlogs(DocumentReference blogConfDocRef
+      ) throws QueryException, XWikiException;
   
   /**
    * gets articles as list for given {@link ArticleSearchParameter}
    * 
+   * @param blogConfDocRef may not be null
    * @param param may not be null
    * @return
    * @throws ArticleLoadException if there was an error loading articles
    */
-  public List<Article> getArticles(ArticleSearchParameter param
-      ) throws ArticleLoadException;
+  public List<Article> getArticles(DocumentReference blogConfDocRef, 
+      ArticleSearchParameter param) throws ArticleLoadException;
 
 }
